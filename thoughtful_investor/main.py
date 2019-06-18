344d5bb57168459cddb41ce02cab6df4ebbbf4c4
etienne-napoleone <etienne@tomochain.com>
2019-06-19 03:51:08 +0700

    feat: add telegram key argument

--- thoughtful_investor/main.py
+++ thoughtful_investor/main.py
@@ -4,7 +4,9 @@ import click
 
 
 @click.command(help='A telegram bot holding TOMO.')
+@click.argument('telegram_key', envvar='TELEGRAM_KEY')
+@click.option('--debug', is_flag=True, help='Set logging level to debug.')
 @click.version_option(version=__version__)
-def entrypoint():
+def entrypoint(telegram_key, debug):
     """CLI entrypoint"""
     pass
